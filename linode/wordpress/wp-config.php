f37cc4038aa4e383c53f1ca8d1932642c70ea24c
oculus <oculus@gmail.com>
2017-09-21 19:33:36 +0530

    changed wp-config setting

--- linode/wordpress/wp-config.php
+++ linode/wordpress/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'DBNAME');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'DBUSER');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'pass');
+define('DB_PASSWORD', 'DBPASS');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
